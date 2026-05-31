0a1268d62a81d5689c8efd8924425ac03f74ccf8
Johan Sánchez Vásquez <johannsava09@gmail.com>
2026-05-30 18:29:40 -0600

    Primera parte - UI

---
+++ Tienda/Controllers/CuentaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tienda.Controllers
+{
+    public class CuentaController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public ActionResult Recuperar()
+        {
+            return View();
+        }
+    }
+}
